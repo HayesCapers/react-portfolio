a5d6c441327a546e4b585f97606c48ba08da7097
Hayes Capers <capersj5@yahoo.com>
2017-09-01 00:05:50 -0400

    moved project contents to it qown component

---
+++ src/components/ProjectContent.js
@@ -0,0 +1,76 @@
+import React, { Component } from 'react';
+import $ from 'jquery';
+
+class ProjectContent extends Component {
+	constructor(props) {
+		super(props);
+		
+		this.state = {
+			
+		}
+	}
+
+	mouseOn(){
+		$('#wnd-2').children('img').removeClass('img-2-out');
+		$('#wnd-3').children('img').removeClass('img-3-out');
+		$('#wnd-4').children('img').removeClass('img-4-out');
+
+		$('#wnd-2').children('img').addClass('img-2-in');
+		$('#wnd-3').children('img').addClass('img-3-in');
+		$('#wnd-4').children('img').addClass('img-4-in');
+
+		$('.cls-1').addClass('draw');
+		$('.demo-link').addClass('demo-draw');
+	}
+
+	mouseOff(){
+		$('#wnd-2').children('img').addClass('img-2-out');
+		$('#wnd-3').children('img').addClass('img-3-out');
+		$('#wnd-4').children('img').addClass('img-4-out');
+
+		$('#wnd-2').children('img').removeClass('img-2-in');
+		$('#wnd-3').children('img').removeClass('img-3-in');
+		$('#wnd-4').children('img').removeClass('img-4-in');
+
+		$('.cls-1').removeClass('draw');
+		$('.demo-link').removeClass('demo-draw');
+	}
+
+	render() {
+
+		const images = [
+			'Wnd.png'
+		]
+		const titles = [
+			'WINE AND DINE'
+		]
+		var currPrj = this.props.projId
+		console.log(currPrj)
+
+		return(
+
+			<div className='main-wrapper' onMouseEnter={this.mouseOn} onMouseLeave={this.mouseOff}>
+				<svg className="border-line" id="layer_1" data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 798.04 470.77">
+				  <polyline id="line-1" className="cls-1" points="797.67 432.73 797.67 0.38 0.38 0.38 0.38 459.93 729.25 459.93 735.64 447.32 736.55 469.21 742.02 457.35 757.52 457.35"/>
+				</svg>
+
+				<svg className="border-line-2" id="layer_2" data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 798.04 470.77">
+				  <polyline id="line-2" className="cls-1" points="797.67 432.73 797.67 0.38 0.38 0.38 0.38 459.93 729.25 459.93 735.64 447.32 736.55 469.21 742.02 457.35 757.52 457.35"/>
+				</svg>
+
+				<div className="wnd-main" id="wnd-1"><img src={`/images/${images[currPrj]}`} /></div>
+				<div className="wnd-main" id="wnd-2"><img src={`/images/${images[currPrj]}`} className="img-2-out" /></div>
+				<div className="wnd-main" id="wnd-3"><img src={`/images/${images[currPrj]}`} className="img-3-out" /></div>
+				<div className="wnd-main" id="wnd-4"><img src={`/images/${images[currPrj]}`} className="img-4-out" /></div>
+
+				<div className='title-wrapper'>{titles[currPrj]}</div>
+				<div className='demo-link'>DEMO</div>
+			</div>
+
+		)
+	}
+}
+
+
+export default ProjectContent
+
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import ProjectContent from './ProjectContent';
 import $ from 'jquery';
 
 
@@ -8,42 +9,9 @@ class Projects extends Component {
 		super(props);
 	}
 
-	mouseOn(){
-		$('#wnd-2').children('img').removeClass('img-2-out');
-		$('#wnd-3').children('img').removeClass('img-3-out');
-		$('#wnd-4').children('img').removeClass('img-4-out');
-
-		$('#wnd-2').children('img').addClass('img-2-in');
-		$('#wnd-3').children('img').addClass('img-3-in');
-		$('#wnd-4').children('img').addClass('img-4-in');
-
-		$('.cls-1').addClass('draw');
-		$('.demo-link').addClass('demo-draw');
-	}
-
-	mouseOff(){
-		$('#wnd-2').children('img').addClass('img-2-out');
-		$('#wnd-3').children('img').addClass('img-3-out');
-		$('#wnd-4').children('img').addClass('img-4-out');
-
-		$('#wnd-2').children('img').removeClass('img-2-in');
-		$('#wnd-3').children('img').removeClass('img-3-in');
-		$('#wnd-4').children('img').removeClass('img-4-in');
-
-		$('.cls-1').removeClass('draw');
-		$('.demo-link').removeClass('demo-draw');
-	}
+	
 
 	render() {
-
-		const images = [
-			'Wnd.png'
-		]
-		const titles = [
-			'WINE AND DINE'
-		]
-		var currPrj = this.props.match.params.prjId
-
 		return(
 			<div className='body-wrapper'>
 				{this.loadScript}
@@ -53,32 +21,14 @@ class Projects extends Component {
 					</div>
 				</Link>
 
-				<div className='main-wrapper' onMouseEnter={this.mouseOn} onMouseLeave={this.mouseOff}>
-					<svg className="border-line" id="layer_1" data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 798.04 470.77">
-					  <polyline id="line-1" className="cls-1" points="797.67 432.73 797.67 0.38 0.38 0.38 0.38 459.93 729.25 459.93 735.64 447.32 736.55 469.21 742.02 457.35 757.52 457.35"/>
-					</svg>
-
-					<svg className="border-line-2" id="layer_2" data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 798.04 470.77">
-					  <polyline id="line-2" className="cls-1" points="797.67 432.73 797.67 0.38 0.38 0.38 0.38 459.93 729.25 459.93 735.64 447.32 736.55 469.21 742.02 457.35 757.52 457.35"/>
-					</svg>
-
-					<div className="wnd-main" id="wnd-1"><img src={`/images/${images[currPrj]}`} /></div>
-					<div className="wnd-main" id="wnd-2"><img src={`/images/${images[currPrj]}`} className="img-2-out" /></div>
-					<div className="wnd-main" id="wnd-3"><img src={`/images/${images[currPrj]}`} className="img-3-out" /></div>
-					<div className="wnd-main" id="wnd-4"><img src={`/images/${images[currPrj]}`} className="img-4-out" /></div>
-
-					<div className='title-wrapper'>{titles[currPrj]}</div>
-					<div className='demo-link'>DEMO</div>
-				</div>
+				<ProjectContent projId={this.props.match.params.prjId}/>
 
 				<div className='prj-nav-wrapper'>	
-					<Link to='/projects/0'>
-						<div className='prj-nav-button'>
-							<div className='nums' id='num1'>1</div>
-							<img className='slash' src='/images/slash.png'/>
-							<div className='nums' id='num2'>3</div>
-						</div>
-					</Link>	
+					<div className='prj-nav-button'>
+						<div className='nums' id='num1'>1</div>
+						<img className='slash' src='/images/slash.png'/>
+						<div className='nums' id='num2'>3</div>
+					</div>
 				</div>
 			</div>
 		)
